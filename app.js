81b624324f802e6da6527b7e7c19492cf913de9a
jeyadevi <jeyadevikj04@gmail.com>
2024-02-13 13:33:23 +0530

    Fix

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+
+app.use(express.static(path.join(__dirname)));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
